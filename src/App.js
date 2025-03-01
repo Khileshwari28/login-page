918bb2c1d7b30c1cd71e8b71b3b315c34ca4acb2
Khileshwari Deshmukh <140846427+Khileshwari28@users.noreply.github.com>
2025-03-01 22:18:39 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import LoginForm from './component/LoginForm/LoginForm';
+
+function App() {
+  return (
+    <div >
+      <LoginForm />
+    </div>
+  );
+}
+
+export default App;
